c4506598465167f050d65991125396f7315b6e40
Vlad Tyrtychnyi <tva100702@gmail.com>
2022-02-23 14:44:32 +0300

    feat(App): Base structure added

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {Provider} from 'react-redux';
+import {PersistGate} from "redux-persist/integration/react";
+import store, {persistor} from "./store";
+import {BrowserRouter} from "react-router-dom";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <PersistGate persistor={persistor} loading={null}>
+                    <App/>
+                </PersistGate>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
---
+++ src/store/index.ts
@@ -0,0 +1,37 @@
+import {configureStore, ThunkAction, Action} from '@reduxjs/toolkit';
+import rootReducer, {RootState} from "./slices";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+export const persistor = persistStore(store)
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
+    RootState,
+    unknown,
+    Action<string>>;
+export default store
